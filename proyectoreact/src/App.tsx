44c3f4f1f84413348e0f5d28a831fcfccf3a7cdd
Pausrq27 <masanchezro@est.utn.ac.cr>
2025-08-09 04:23:21 -0600

    sd

--- proyectoreact/src/App.tsx
+++ proyectoreact/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import RegistrosEntrega from "./pages/RegistrosEntrega";
 import AnalizarQR from "./pages/AnalizarQR";
@@ -6,10 +7,16 @@ import AnalizarQR from "./pages/AnalizarQR";
 export default function App() {
   return (
     <Routes>
+      {/* Página inicial */}
+      <Route path="/" element={<Home />} />
+
+      {/* Páginas secundarias */}
       <Route path="/login" element={<Login />} />
       <Route path="/registrosentrega" element={<RegistrosEntrega />} />
       <Route path="/analizarqr" element={<AnalizarQR />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
+
+      {/* Si la ruta no existe → manda al Home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
